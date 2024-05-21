d09a044e16147e4ffc998edce85dfbff9ab81042
Sofia Rodriguez del Busto <rdbsofi@gmail.com>
2024-05-21 12:51:36 -0300

    Punto 1 completo

---
+++ Calculadora.cs
@@ -0,0 +1,35 @@
+namespace EspacioCalculadora
+{   
+    public class Calculadora
+    {
+        double dato;
+        public double Resultado{get=>dato;}
+        public void Sumar(double termino)
+        {
+           dato += termino; 
+        }
+        public void Restar(double termino)
+        {
+            dato -= termino;
+        }
+        public void Multiplicar(double termino)
+        {
+            dato *= termino;
+        }
+        public void Dividir(double termino)
+        {
+            if(termino != 0)
+            {
+                dato *= termino;
+            }else
+            {
+                Console.WriteLine("Error! Division en 0");
+            }
+        }
+        public void Limpiar()
+        {
+            dato = 0;
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EspacioCalculadora;
+
+Calculadora miCalculadora = new Calculadora();
+
+int control;
+do
+{
+    Console.WriteLine("\n--------Calculadora--------\n");
+    Console.WriteLine("\n1.SUMA");
+    Console.WriteLine("\n2.RESTA");
+    Console.WriteLine("\n3.MULTIPLICACION");
+    Console.WriteLine("\n4.DIVISION");
+    Console.WriteLine("\n5.LIMPIAR");
+    Console.WriteLine("\nElija la operacion que desea realizar: ");
+    string operacion = Console.ReadLine(); 
+    int op;
+    double numero = 0;
+    if(int.TryParse(operacion, out op))
+    {
+        if (op != 5)
+        {
+            string n;
+            do{
+                Console.WriteLine("\nIngrese el numero: ");
+                n = Console.ReadLine(); 
+            }while (!double.TryParse(n, out numero));
+            
+        }
+        switch (op)
+        {
+            case 1:
+                    miCalculadora.Sumar(numero);
+                    Console.WriteLine("\nLa suma de los numeros es " + miCalculadora.Resultado);
+                    break;
+            case 2:
+                    miCalculadora.Restar(numero);
+                    Console.WriteLine("\nLa diferencia de los numeros es " + miCalculadora.Resultado);
+                    break;
+
+            case 3:
+                    miCalculadora.Multiplicar(numero);
+                    Console.WriteLine("\nEl producto de los numeros es " + miCalculadora.Resultado);
+                    break;
+            case 4:
+                    miCalculadora.Dividir(numero);
+                    Console.WriteLine("\nEl cociente de los numeros es " + miCalculadora.Resultado);
+                    break;
+            case 5:
+                    miCalculadora.Limpiar();
+                    Console.WriteLine("\nEl dato vacio es: ", miCalculadora.Resultado);
+                    break;
+
+         }
+    }
+    Console.WriteLine("\nIngrese 1 si desea continuar operando. Caso contrario ingrese 0");
+    string controla = Console.ReadLine();
+    if(!int.TryParse(controla, out control))
+    {
+      control = 1;
+    }
+    
+} while (control != 0);
